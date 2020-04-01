4d2583b8f858413f02587523b87b075a78404432
Latricia <tricianickelberry@gmail.com>
2020-04-01 16:24:45 -0700

    added db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,13 @@
 # Here is where I will create some seed data to work with and test associations
+
+# Create 2 users
+
+latricia = User.create(name: "Latricia", email: "Latricia@Latricia.com", password: "password")
+tricia = User.create(name: "tricia", email: "tricia@tricia.com", password: "password")
+
+# Create some Gratitude entries
+
+GratitudeEntry.create(content: "I am thankful for my job", user_id: latricia.id)
+
+# Use Activerecord to pre-associate data:
+latricia.gratitude_entries.create(content: " I am building using sinatra")
