3b9b4f11402a9819f3e10b4602b30cab61c7c2c0
Latricia <tricianickelberry@gmail.com>
2020-04-01 19:27:35 -0700

    updated user controller, login view error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "amazing_gratitude_app"
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,38 @@ class UsersController < ApplicationController
     # The purpose of this route is to? Is to render the login page (form)
     get '/login' do
         erb :login
-
     end
+    # The purpose of this route is to receive the login form, 
+    # Find the user, and log the user in(create a session)
+    post '/login' do
+    # params looks like: {email: "user@user.com", password: "password"}
+    # find the user
+    @user = User.find_by(email:params[:email])
+    # authenticate the user- verify the user is who they say they are
+    # They have the credentials email/password
+   if @user.authenticate(params[:password])
+     # log the user in- create user session
+     session[:user_id] = @user.id # logging the user in
+    # redirect to the users landing page(show?, index?, dashboard)
+    
+   else
+    # tell the user they entered in invalid credentials
+    # redirect to login page
+    puts session
+    redirect "users/#{@user.id}"
+   
+    end
+end
 
     # What routes do I need for signup?
 
     get '/signup' do
- end
+  end
+
+ # users show route
+
+   get '/users/:id' do
+    
+    "this will be the users show route"
+  end
 end
