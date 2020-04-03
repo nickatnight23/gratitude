436931972e86c02ef999df86380ec1a38006396e
Latricia <tricianickelberry@gmail.com>
2020-04-02 19:27:32 -0700

    current user and logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,18 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers  do
+
+    def logged_in?
+      # true if user is logged in, otherwise false
+
+      !!current_user
+
+    end
+
+    def current_user
+     @current_user || = user.find_by(:id session[:user_id])
+
+    end
+ 
 end
