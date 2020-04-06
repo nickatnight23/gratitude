6535d22994b5acf4a1de71cadf1a5a18e7b391fb
Latricia <tricianickelberry@gmail.com>
2020-04-06 08:18:55 -0700

    updated gratitude controller

--- app/controllers/gratitude_entries_controller.rb
+++ app/controllers/gratitude_entries_controller.rb
@@ -8,11 +8,29 @@ class GratitudeEntriesController < ApplicationController
 
     # post gratitude_entries to create a new gratitude entry
 
-    post '/gratitude_entries' do
+    post '/Gratitude_entries/gratitude_entries' do
+        
+        
+        # I want to create a new gratitude entry and save it to the DB
+        # I also only want to create a gratitude entry if a user is logged in
+        if !logged_in?
+
+            redirect '/'
+        end
+        # I only want to save if it has some content
+        if params[:content] != ""
+        # create a new entry
+        @gratitude_entry = GratitudeEntry.create(content: params[:content],
+         user_id: current_user.id)
+         redirect "/gratitude_entries/#{@gratitude_entry.id}"
+
+        else
+        redirect '/gratitude_entries/new'
         
     end
 
     # show route for gratitude entry
 
     # index route for all gratitude entries
+end
 end
