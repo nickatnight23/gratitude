efa704debb576e9cce960d51c8cdff0976bb3f17
Latricia <tricianickelberry@gmail.com>
2020-04-06 07:38:32 -0700

    created new form, mounted controller

--- app/controllers/gratitude_entries_controller.rb
+++ app/controllers/gratitude_entries_controller.rb
@@ -1,11 +1,16 @@
 class GratitudeEntriesController < ApplicationController
     
     # get gratitude_entries/ new to render a form to create new entry
-    get '/gratitude_entries/new' do
+    get '/Gratitude_entries/new' do
+
         erb :'/gratitude_entries/new'
     end
 
     # post gratitude_entries to create a new gratitude entry
+
+    post '/gratitude_entries' do
+        
+    end
 
     # show route for gratitude entry
 
