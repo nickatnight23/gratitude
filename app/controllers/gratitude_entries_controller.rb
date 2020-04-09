bddea8602663dba65c280f164be2ad2563ea935b
Latricia <tricianickelberry@gmail.com>
2020-04-09 16:40:35 -0700

    updated delete controller

--- app/controllers/gratitude_entries_controller.rb
+++ app/controllers/gratitude_entries_controller.rb
@@ -86,8 +86,18 @@ end
       end
     end
 
+        delete '/gratitude_entries/:id' do
+            set_gratitude_entry
+            if authorized_to_edit?(@gratitude_entry)
+                @gratitude_entry.destroy
+                rediret '/gratitude_entries'
+                # delete the entry
+                # go some where
+            else
+                rediret '/gratitude_entries'
 
-    # index route for all gratitude entries
+            end
+        end
 
     private
 
